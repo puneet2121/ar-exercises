48850c66e2d175beb90c78a67424590df630f7ac
puneet2121 <bedipuneet21@gmail.com>
2022-06-29 19:32:59 +0000

    Solve ex4 and ex5

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "Surrey", annual_revenue: 224000,mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue: 1900000,womens_apparel: false, mens_apparel: true)
+store = Store.create(name: "Yaletown", annual_revenue: 430000,mens_apparel: true, womens_apparel: true)
+
+@men_store = Store.where(mens_apparel:true)
+puts @men_store
+
+store = Store.new do |s|
+  puts s.name 
+  s.annual_revenue
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+sum = Store.pluck(:annual_revenue).sum
+puts sum
+average = Store.pluck(:annual_revenue).sum/(Store.count)
+puts average
+store = Store.where("annual_revenue > ?" , 1000000).count
+puts store
